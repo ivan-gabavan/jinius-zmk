ed9d47add159e6d543dca3dc88bad247fef4110c
ivan-gabavan <53791165+ivan-gabavan@users.noreply.github.com>
2024-12-23 15:41:05 +0300

    Create keys_ru.h
    Signed-off-by: ivan-gabavan <53791165+ivan-gabavan@users.noreply.github.com>

---
+++ config/keys_ru.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <dt-bindings/zmk/keys.h>
+
+// Русская раскладка ЙЦУКЕН
+
+// Верхний ряд
+#define RU_YO    (LS(NUBS))    // Ё
+#define RU_1     (N1)          // 1
+#define RU_2     (N2)          // 2
+#define RU_3     (N3)          // 3
+#define RU_4     (N4)          // 4
+#define RU_5     (N5)          // 5
+#define RU_6     (N6)          // 6
+#define RU_7     (N7)          // 7
+#define RU_8     (N8)          // 8
+#define RU_9     (N9)          // 9
+#define RU_0     (N0)          // 0
+#define RU_MINUS (MINUS)       // -
+#define RU_EQUAL (EQUAL)       // =
+
+// Средний ряд
+#define RU_Q     (Q)           // Й
+#define RU_W     (W)           // Ц
+#define RU_E     (E)           // У
+#define RU_R     (R)           // К
+#define RU_T     (T)           // Е
+#define RU_Y     (Y)           // Н
+#define RU_U     (U)           // Г
+#define RU_I     (I)           // Ш
+#define RU_O     (O)           // Щ
+#define RU_P     (P)           // З
+#define RU_LBKT  (LBKT)        // Х
+#define RU_RBKT  (RBKT)        // Ъ
+
+// Нижний ряд
+#define RU_A     (A)           // Ф
+#define RU_S     (S)           // Ы
+#define RU_D     (D)           // В
+#define RU_F     (F)           // А
+#define RU_G     (G)           // П
+#define RU_H     (H)           // Р
+#define RU_J     (J)           // О
+#define RU_K     (K)           // Л
+#define RU_L     (L)           // Д
+#define RU_SCLN  (SCLN)        // Ж
+#define RU_QUOT  (QUOT)        // Э
+#define RU_BSLH  (BSLH)        // \ (обратный слэш)
+
+// Нижний ряд
+#define RU_Z     (Z)           // Я
+#define RU_X     (X)           // Ч
+#define RU_C     (C)           // С
+#define RU_V     (V)           // М
+#define RU_B     (B)           // И
+#define RU_N     (N)           // Т
+#define RU_M     (M)           // Ь
+#define RU_COMMA (COMMA)       // Б
+#define RU_DOT   (DOT)         // Ю
+#define RU_SLASH (SLASH)       // . (точка)
